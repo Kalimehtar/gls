e912110175cde2b4f032488566dfe62ad218cbe0
Kalimehtar <kalimehtar@mail.ru>
2015-12-23 15:12:41 +0500

    Fix deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,4 @@
 #lang setup/infotab
-(define version "0.8")
+(define version "0.9")
 (define collection 'multi)
-(define deps '("base" "srfi-lite-lib"))
+(define deps '("base" "srfi-lite-lib" "rackunit-lib"))
